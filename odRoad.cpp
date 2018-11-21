7e10c707f91a84d1a7bcdc03a4749f1cea67428e
miguelleitao <jml@isep.ipp.pt>
2018-11-21 17:29:01 +0000

    first version

---
+++ odRoad.cpp
@@ -0,0 +1,167 @@
+/*
+ * odRoad
+ *
+ */
+
+#include <math.h>
+#include <cassert>
+#include "odRoad.h"
+
+int verbose = 0;
+
+void CopyVecD(double *v, double *dest) {
+        int i;
+        for( i=0 ; i<3 ; i++ )
+                dest[i] = v[i];
+}
+
+int GetLine(char **line, size_t *line_len, FILE *fin) {
+        int nc = getline( line, line_len, fin);
+        return nc;
+}
+
+int PlanView::loadPts(const char *fname) {
+	FILE *fin;
+    long vertexes = 0;
+    long comments = 0;
+    long invalid = 0;
+    long lines = 0;
+    double length;
+    double total_length = 0.;
+    int file_format = 6;
+    double p[3], e[3];
+    //double last_v[3];
+    double last_p[3] = {0.,0.,0.};
+    //double v[3];
+	char *line = (char *) malloc (PTS_MAX_LINE_SIZE + 1);
+	size_t line_len = PTS_MAX_LINE_SIZE;
+
+	fin = fopen(fname,"r");
+	if ( ! fin ) {
+		fprintf(stderr, "Cannot open file '%s'\n", fname);
+		return -1;
+	}
+	int done = false;
+	while( ! done && ! feof(fin)) {
+		int res = GetLine(&line, &line_len, fin);
+		if ( feof(fin) ) break;
+		if ( line[0]=='#' ) {
+			comments++;
+			continue;
+		}
+		if ( file_format==6 ) {
+			res = sscanf(line,"%lf %lf %lf %lf %lf %lf\n", p+0, p+1, p+2, e+0, e+1, e+2);
+			lines++;
+			if ( verbose>=4 ) {
+				printf("res %d, %lf %lf %lf\n",res,p[0],p[1],p[2]);
+			}
+			if ( res<3 ) {
+				invalid++;
+				if ( verbose>1 )
+				  printf("Invalid line %ld: %s\n", lines, line);
+				continue;
+			}
+			vertexes++;
+			if ( res<6 ) e[0] = e[1] = e[2] = 0.;
+		}
+		if ( file_format==1 ) {
+			if ( sscanf(line,"%lf", p+1) <1 ) {
+				invalid++;
+				continue;
+			}
+			p[0] = (double)vertexes;
+			vertexes++;
+		}
+		length = sqrt( (p[0]-last_p[0])*(p[0]-last_p[0]) + (p[1]-last_p[1])*(p[1]-last_p[1]) );
+
+		odrGeometry *geoNode = new odrLine();
+		geoNode->s = total_length;
+		geoNode->x = p[0];
+		geoNode->y = p[1];
+		geoNode->hdg = e[0];
+		geoNode->length = length;
+		push_back(*geoNode);
+		
+		total_length += length;
+
+		CopyVecD(p,last_p);
+	}
+	printf("leu %ld segs\n", vertexes);
+	return vertexes;
+}
+
+int PlanView::saveXodr(xmlWriter xmlFile) {
+	unsigned i;	
+	int rc=0;
+	
+	double total_length = 0.;
+	xmlFile.writeElement("planView");
+	for( i=0 ; rc>=0 && i<size() ; i++ ) {
+		/* Start an element named "geometry" as child of planView. */
+		rc = xmlFile.writeElement("geometry");
+
+		/* Add an attribute with name "s" and value "1.0" to ORDER. */
+		rc = xmlFile.writeAttributeDouble("s", total_length);
+
+		odrGeometry geoNode = at(i);
+		
+		xmlFile.writeAttributesXY( geoNode.x, geoNode.y);
+		xmlFile.writeAttributeDouble("hdg", geoNode.hdg);
+		xmlFile.writeAttributeDouble("length", geoNode.length);
+
+		/* Start an element named "line" as child of geometry. */
+		rc = xmlFile.writeElement("line");
+	
+		/* Close the element named line. */
+		rc = xmlFile.closeElement();
+
+		total_length += geoNode.length;
+		assert( abs(total_length-geoNode.x)<0.001 );
+		/* Close the element named geometry. */
+		rc = xmlFile.closeElement();
+	}
+	printf("gravou %u segs\n",i);
+	return i;
+}
+
+
+int PlanView::print() {
+	unsigned i;
+	
+	double total_length = 0.;
+	printf("planView\n");
+	for( i=0 ; rc>=0 && i<size() ; i++ ) {
+		/* Start an element named "geometry" as child of planView. */
+		printf("  geometry\n");
+
+		/* Add an attribute with name "s" and value "1.0" to ORDER. */
+		printf("    s: %lf", total_length);
+
+		odrGeometry geoNode = at(i);
+		
+		printf("    x,y: %lf, %lf\n", 	geoNode.x, geoNode.y);
+		printf("    hdg: %lf\n",	geoNode.hdg);
+		printf("    length: %lf\n", 	geoNode.length);
+
+		total_length += geoNode.length;
+		assert( abs(total_length-geoNode.x)<0.001 );
+	}
+	printf("gravou %u segs\n",i);
+	return i;
+}
+
+
+int odRoad::loadXodr(string &fname) {
+
+	return 0;
+}
+
+
+
+void odRoad::print() {
+};
+
+
+
+
+
---
+++ odRoad.h
@@ -0,0 +1,191 @@
+/*
+ * odRoad
+ *
+ */
+
+#include <string>
+#include <vector>
+#include <libxml/encoding.h>
+#include <libxml/xmlwriter.h>
+
+
+#define MY_ENCODING "ISO-8859-1"
+#define PTS_MAX_LINE_SIZE (80)
+
+using namespace std;
+
+typedef double scalar;
+
+#define GEOMETRY_LINE 	(220)
+#define GEOMETRY_ARC 	(221)
+#define GEOMETRY_SPIRAL	(222)
+
+class odrGeometry {
+    public:
+	odrGeometry() {
+		type = 0;
+		s = 0.;
+		x = 0.;
+		y = 0.;
+		hdg = 0.;
+		length = 0.;
+	}
+	unsigned char type;
+	scalar s;
+	scalar x;
+	scalar y;
+	scalar hdg;
+	scalar length;
+};
+
+class odrLine : public odrGeometry {
+    public:
+	odrLine() {
+		type = GEOMETRY_LINE;
+	}
+};
+class odrArc : public odrGeometry {
+    public:
+	scalar curv;
+	odrArc() {
+		type = GEOMETRY_ARC;
+		curv = 0.;
+	}
+};
+class odrSpiral : public odrGeometry {
+    public:
+	scalar curvIn;
+	scalar curvOut;
+	odrSpiral() {
+		type = GEOMETRY_SPIRAL;
+		curvIn = 0.;
+		curvOut = 0.;
+	}
+};
+
+//typedef vector<Geometry> PlanView;
+
+class PlanView : public vector<odrGeometry> {
+    public:
+	int loadPts(const char * fname);
+	int savePts(const char * fname);
+	int saveXodr(class xmlWriter);
+};
+
+class odRoad {
+	string name;
+	scalar length;
+	int id;
+	int junction;
+
+	PlanView planView;
+
+	odRoad() {
+		name = "";
+		length = 0.;
+		id = -1;
+		junction = 0;
+	}
+	int loadXodr(string &fname);
+	int saveXodr(string &fname);
+	int loadPts(const char*fname) {
+		return planView.loadPts(fname);
+	}
+	void print();
+};
+
+
+class xmlWriter {
+    public:
+	const char* fname;
+	xmlDocPtr xmlDoc;
+	xmlTextWriterPtr xmlFile;
+	xmlWriter() {
+		fname = "";
+	}
+	void open(char *file_name) {
+		fname = file_name;
+		xmlFile = xmlNewTextWriterDoc(&xmlDoc, 0);
+
+  		if ( xmlFile == NULL ) {
+        		fprintf(stderr, "xmlWriter: Error creating the xml writer\n");
+        		return;
+		}
+		/* Start the document with the xml default for the version,
+    		 * encoding ISO 8859-1 and the default for the standalone
+    		 * declaration. */
+    		int rc = xmlTextWriterStartDocument(xmlFile, NULL, MY_ENCODING, NULL);
+    		if (rc < 0) {
+        		printf("testXmlwriterDoc: Error at xmlTextWriterStartDocument\n");
+        		return;
+    		}	
+
+    	}
+	xmlWriter(char *file_name) {
+		open(file_name);
+	}
+
+	void close() {
+		printf("gravou tudo\n");
+    		xmlFreeTextWriter(xmlFile);
+
+    		printf("libertou writer\n");
+    		int nb = xmlSaveFileEnc(fname, xmlDoc, MY_ENCODING);
+    		if ( nb>=0 )	fprintf(stderr, "File saved with %d Bytes\n", nb);
+    		else		fprintf(stderr, "Error saving file\n");
+    		xmlFreeDoc(xmlDoc);
+
+    		printf("libertou tudo\n");
+	}
+	int writeElement(const char* eName) {
+		int rc;
+		/* Start an element named "eName" as child of current element. */
+		rc = xmlTextWriterStartElement(xmlFile, BAD_CAST eName);
+		if (rc < 0) 
+			fprintf(stderr, "Error at xmlWriteElement, writing element '%s'\n", eName);
+		return rc;
+	}
+	int writeAttribute(const char*  aName, const char*  aVal) {
+    		/* Add an attribute with name aName and value aVal to current element. */
+    		int rc;
+    		rc = xmlTextWriterWriteAttribute(xmlFile, BAD_CAST aName, BAD_CAST aVal);
+    		if (rc < 0) 
+			fprintf(stderr,"error writing attribute '%s'\n", aName);
+		return rc;
+	}
+	int writeAttributeInt(const char* aName, int aVal) {
+    		/* Add an attribute with name aName and value v to current element. */
+    		int rc;
+    		char vStr[180];
+    		sprintf(vStr,"%d",aVal);
+    		rc = writeAttribute( aName, vStr);
+    		return rc;
+	}
+	int writeAttributeDouble(const char* aName, double aVal) {
+    		/* Add an attribute with name aName and value v to current element. */
+    		int rc;
+    		char vStr[180];
+    		sprintf(vStr,"%lf",aVal);
+    		rc = writeAttribute( aName, vStr);
+    		return rc;
+	}
+	int writeAttributesXY(scalar x, scalar y) {
+    		/* Add an attribute with name aName and value v to current element. */
+		int rc;
+    		rc = writeAttributeDouble( "x", x);
+    		rc = writeAttributeDouble( "y", y);
+    		return rc;
+	}
+	int closeElement() {
+		/* Close the current element. */
+		int rc;
+		rc = xmlTextWriterEndElement(xmlFile);
+		if (rc < 0) {
+			printf("testXmlwriterDoc: Error at xmlTextWriterEndElement\n");
+			return -1;
+		}
+		return rc;
+	}
+
+};
+
